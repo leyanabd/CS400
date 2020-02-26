1294dcbc37b88c0c5e00c0651b38e9c5192160b6
leyanabd <47067237+leyanabd@users.noreply.github.com>
2020-02-26 14:21:19 -0500

    Add files via upload
    Edited routes/ps3

---
+++ ps3.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* Get fixed string */
+router.get('/', function(req, res, next) {
+    res.render('ps3', {title: 'Hey! How are you doing?'});
+});
+
+/* POST string and length */
+router.post('/', function(req, res, next) {
+    const ps = {
+        string: req.body.title,
+        length: req.body.title.length,
+    };
+    console.log(ps);
+    res.render('ps3', {title: JSON.stringify(ps)});
+});
+
+module.exports = router;
