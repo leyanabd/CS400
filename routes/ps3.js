ed79cb51558b812d526df0d30749561e5db16e15
leyanabd <47067237+leyanabd@users.noreply.github.com>
2020-02-26 21:31:49 -0500

    Update ps3.js
    Final Edits

--- routes/ps3.js
+++ routes/ps3.js
@@ -3,17 +3,12 @@ var router = express.Router();
 
 /* GET fixed string */
 router.get('/', function(req, res, next) {
-    res.render('ps3', {title: 'Hey! How are you doing?'});
+    res.render('ps3', {string: 'Hey! How are you doing?'});
 });
 
 /* POST string and length */
 router.post('/', function(req, res, next) {
-    const ps = {
-        string: req.body.title,
-        length: req.body.title.length,
-    };
-    console.log(ps);
-    res.render('ps3', {title: JSON.stringify(ps)});
+    res.render('ps3', {string: req.body.title, length: req.body.title.length});
 });
 
 module.exports = router;
